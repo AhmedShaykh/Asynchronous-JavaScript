6424fc0cefe47a06fb6aac4084098423da453a43
Ahmed Saleem Shaikh <98047091+AhmedShaykh@users.noreply.github.com>
2022-02-28 22:20:51 +0500

    It's Time To Push !

--- App.js
+++ App.js
@@ -1,85 +1,85 @@
-// console.log('Start Learning JS !');
+console.log('Start Learning JS !');
 
-// let data = "Learning Asynchronous JavaScript + ES6 (Vanilla Javascript)";
-// document.write(data)
+let data = "Learning Asynchronous JavaScript + ES6 (Vanilla Javascript)";
+document.write(data)
 
-// let learn = "Content";
+let learn = "Content";
 
-// setTimeout(function () {
-//     console.log(learn)
-// }, 1000);
+setTimeout(function () {
+    console.log(learn)
+}, 1000);
 
-// console.log('End Learning JS !');
+console.log('End Learning JS !');
 
-// console.log('Office Time');
+console.log('Office Time');
 
-// function backHome() {
-//     setTimeout(function () {
-//         console.log("Brain Working Stop");
-//     }, 3000)
-// }
+function backHome() {
+    setTimeout(function () {
+        console.log("Brain Working Stop");
+    }, 3000)
+}
 
-// function workTime() {
-//     console.log("Brain Working Start");
-//     backHome()
-// }
-// workTime();
+function workTime() {
+    console.log("Brain Working Start");
+    backHome()
+}
+workTime();
 
-// console.log('Sleeping Time');
+console.log('Sleeping Time');
 
-// function printToken(msg, alertToken) {
-//     alertToken(msg)
-// }
+function printToken(msg, alertToken) {
+    alertToken(msg)
+}
 
-// function alertToken(msg) {
-//     alert(msg);
-// }
+function alertToken(msg) {
+    alert(msg);
+}
 
-// printToken("Hello Crypto", alertToken);
+printToken("Hello Crypto", alertToken);
 
 
-// function printToken(msg, alertToken) {
-//     if (msg === 'Bitcoin') {
-//         btc(alertToken)
-//     }
-//     else if (msg === 'Ethereum') {
-//         eth(alertToken)
-//     }
-//     else {
-//         bnb(alertToken)
-//     }
-// }
+function printToken(msg, alertToken) {
+    if (msg === 'Bitcoin') {
+        btc(alertToken)
+    }
+    else if (msg === 'Ethereum') {
+        eth(alertToken)
+    }
+    else {
+        bnb(alertToken)
+    }
+}
 
-// function btc(alertToken) {
-//     alert("Buy & Sell Bitcoin");
-//     alertToken("Hello Bitcoin")
-// }
+function btc(alertToken) {
+    alert("Buy & Sell Bitcoin");
+    alertToken("Hello Bitcoin")
+}
 
-// function eth(alertToken) {
-//     alert("Buy & Sell Ethereum");
-//     alertToken("Hello Ethereum")
-// }
+function eth(alertToken) {
+    alert("Buy & Sell Ethereum");
+    alertToken("Hello Ethereum")
+}
 
-// function bnb(alertToken) {
-//     alert("Buy & Sell Binance Coin");
-//     alertToken("Hello Binance Coin")
-// }
+function bnb(alertToken) {
+    alert("Buy & Sell Binance Coin");
+    alertToken("Hello Binance Coin")
+}
 
-// function alertToken(msg) {
-//     alert(msg);
-// }
+function alertToken(msg) {
+    alert(msg);
+}
 
-// printToken("Bitcoin", alertToken);
+printToken("Bitcoin", alertToken);
 
-// let promise = new Promise(function (resolve, reject) {
-//     setTimeout(function () {
-//         console.log("Promise Run!")
-//         resolve()
-//     }, 1000)
-// })
-// promise.then(function () {
-//     console.log("Promise Is Resolve")
-// })
+let promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Promise Run!")
+        resolve()
+    }, 1000)
+})
+promise.then(function () {
+    console.log("Promise Is Resolve")
+})
 
 const anyFunction = () => {
     return (((Math.floor(Math.random() * 10)) % 2) === 0) ? true : false
@@ -96,14 +96,14 @@ result.then(resolveFunction).catch(rejectFunction)
 
 console.log("Testing API")
 
-let promise = new Promise(function (resolve, reject) {
+let promiseData = new Promise(function (resolve, reject) {
     setTimeout(function () {
         console.log("New Promise")
         resolve()
     }, 1000)
 })
 
-promise.then(function(){
+promiseData.then(function(){
     console.log("Promise Resolve")
     return 2
 })
