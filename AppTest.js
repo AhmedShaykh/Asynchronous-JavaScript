082e75f2fa76265eda166597d65f9c560cfc6cfd
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-06-09 13:39:30 +0500

    It's Time To Push !

--- AppTest.js
+++ AppTest.js
@@ -1,32 +1,33 @@
 console.log("Before Prepare food");
-function prepareFood(callback){
-    setTimeout(()=>{
+
+function prepareFood(callback) {
+    setTimeout(() => {
         console.log("Prepare Food");
         callback("Food is Ready");
-    },10);
+    }, 10);
 }
-function prepareFrenchToast(callback){
-    setTimeout(()=>{
+function prepareFrenchToast(callback) {
+    setTimeout(() => {
         console.log("Prepare Frensh Toast");
         callback("French Toast is Ready");
-    },20);
+    }, 20);
 }
-function prepareCoffee(callback){
-    setTimeout(()=>{
+function prepareCoffee(callback) {
+    setTimeout(() => {
         console.log("Prepare Coffee");
         callback("Coffee is Ready");
-    },30);
+    }, 30);
 }
-function mycallback(value){
-    console.log("Food is Ready callback = ",value);
+function mycallback(value) {
+    console.log("Food is Ready callback = ", value);
     prepareFrenchToast(frenchToastcallback);
 }
-function frenchToastcallback(value){
-    console.log("French Toast is Ready callback = ",value);
+function frenchToastcallback(value) {
+    console.log("French Toast is Ready callback = ", value);
     prepareCoffee(coffeecallback);
 }
-function coffeecallback(value){
-    console.log("Coffee is Ready callback = ",value);
+function coffeecallback(value) {
+    console.log("Coffee is Ready callback = ", value);
 }
 
 prepareFood(mycallback);
